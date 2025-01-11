dd82232ce0df013e93c1a5b3419c7f6ae189f45a
Eddie <edwardchen_97@hotmail.com>
2025-01-11 11:45:05 +1100

    about in view.py working

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1> Home working </h1>')
+    return HttpResponse('<h1> Task Flow Home working </h1>')
+
+def about(request):
+    return HttpResponse('<h1> Task Flow About page working </h1>')
+    # return render(request, 'about.html')
 
 # Create your views here.
