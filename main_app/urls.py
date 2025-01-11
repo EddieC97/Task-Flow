8aa61fb52abd29766e7bdeb776100252c3e3ed78
Eddie <edwardchen_97@hotmail.com>
2025-01-11 20:40:35 +1100

    ListView working

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,7 +4,7 @@ from .import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('tasks/', views.task_index, name='task-index'),
+    path('tasks/', views.TaskList.as_view(), name='task-index'),
     path('tasks/<int:task_id>/', views.task_detail, name='task-detail'), 
     path('tasks/create/', views.TaskCreate.as_view(), name='task-create'),
     path('tasks/<int:pk>/update/', views.TaskUpdate.as_view(), name='task-update'), 
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Task
 from .forms import TaskForm
@@ -13,11 +15,25 @@ def home(request):
 def about(request):
     return render(request, "about.html")
 
+class TaskList(ListView):
+    model = Task 
+    template_name ='tasks/index.html'
+    context_object_name = 'tasks'
+    
+    extra_context ={'task_form': TaskForm()}
+    
+    #* template_name is useful because by default, Django wil look for a template named task_list.html (following the convention modelname_list.html)
+    #~ Because we are using a more customized template name so we need to specify it 
+    
+    #*  context_object_name is useful because by default, Django will use the lowercase name of the model (task_list) as the context variable name 
+    #~ Because we are using tasks ( a custom name for better clarity), we need to specify it
+    
+    #* extra_context - a simple way to add extra context to the template, usually for static value like a form
 
-def task_index(request):
-    tasks = Task.objects.all()
-    task_form = TaskForm()
-    return render(request, "tasks/index.html", {"tasks": tasks , 'task_form': task_form})
+# def task_index(request):
+#     tasks = Task.objects.all()
+#     task_form = TaskForm()
+#     return render(request, "tasks/index.html", {"tasks": tasks , 'task_form': task_form})
 
 def task_detail(request, task_id):
     task = Task.objects.get(id=task_id)
@@ -26,15 +42,18 @@ def task_detail(request, task_id):
 class TaskCreate(CreateView):
     model = Task
     form_class = TaskForm
-    
+
 
 class TaskUpdate(UpdateView):
     model = Task
     form_class = TaskForm
-    
+
 class TaskDelete(DeleteView):
     model = Task
     success_url= '/tasks/'
     
     # TODO 1. implement task_index + task_detail as CBV
     # TODO 2. try-catch block for better error handling
+
+# * form_class - telling Django to use the custom TaskForm defined in forms.py instead of the default
+# ~ we use this when we need to customize the form beyond what Django's default form generation can handle
