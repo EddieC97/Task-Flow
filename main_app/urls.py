e4ab3c566e35f99f64175a5f040239bdc5f3dc3f
Eddie <edwardchen_97@hotmail.com>
2025-01-11 16:17:09 +1100

    import CreateView in views.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('tasks/', views.task_index, name='task-index'),
-    path('tasks/<int:task_id>/', views.task_detail, name='task-detail' )
+    path('tasks/<int:task_id>/', views.task_detail, name='task-detail'), 
+    path('tasks/create/', views.TaskCreate.as_view(), name='task-create'),
+    
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 from .models import Task
 from django.http import HttpResponse
 
@@ -18,3 +19,7 @@ def task_index(request):
 def task_detail(request, task_id):
     task = Task.objects.get(id=task_id)
     return render(request, 'tasks/detail.html', {'task': task})
+
+class TaskCreate(CreateView):
+    model = Task
+    fields= '__all__'
