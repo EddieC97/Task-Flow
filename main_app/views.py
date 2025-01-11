a62f12426b299d27e2207ccee2bdeacab53b3441
Eddie <edwardchen_97@hotmail.com>
2025-01-11 11:50:09 +1100

    about.html rendering correctly

--- main_app/views.py
+++ main_app/views.py
@@ -5,7 +5,6 @@ def home(request):
     return HttpResponse('<h1> Task Flow Home working </h1>')
 
 def about(request):
-    return HttpResponse('<h1> Task Flow About page working </h1>')
-    # return render(request, 'about.html')
+    return render(request, 'about.html')
 
 # Create your views here.
