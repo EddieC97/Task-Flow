eaa8c3708081379559da184e5f57fff16f933c99
Eddie <edwardchen_97@hotmail.com>
2025-01-11 18:53:59 +1100

    add date widget

--- main_app/forms.py
+++ main_app/forms.py
@@ -5,3 +5,12 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task 
         fields = ['name', 'description', 'date', 'duration', 'progress']
+        widgets = {
+            'date': forms.DateInput(
+                format='%Y-%m-%d',
+                attrs={
+                    'placeholder': 'Select a date',
+                    'type': 'date'
+                }
+            )
+        }
