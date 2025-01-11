b6ee668e44d2f8ea1899b130d22d83ec1a709827
Eddie <edwardchen_97@hotmail.com>
2025-01-11 15:15:08 +1100

    create taskflow db

--- main_app/models.py
+++ main_app/models.py
@@ -9,7 +9,7 @@ STATUS = (
 
 class Task(models.Model):
     name = models.CharField(max_length=250)
-    description = models.Textfield(max_length=250)
+    description = models.TextField(max_length=250)
     date= models.DateField()
     duration= models.IntegerField()
     progress=  models.CharField(max_length=2, choices=STATUS, default=STATUS[0][0])
