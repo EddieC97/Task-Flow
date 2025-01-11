a622c35ca2489cdb045fcea823854bd01569c638
Eddie <edwardchen_97@hotmail.com>
2025-01-11 13:10:21 +1100

    create index.html

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from .import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('tasks/', views.cat_index, name='task-index')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Task:
+    def __init__(self,name, description, date, duration, progress):
+        self.name = name
+        self.description = description
+        self.date = date,
+        self.duration = duration,
+        self.progress = progress
+
+tasks = [
+    Task(
+        "Throw Rubbish", "throw food in the fridge", "11/01/2025", "30mins", "completed"
+    ),
+    Task(
+        "Wash car", "Go to the car wash ", "12/01/2025", "15mins", "unassigned"
+    ),
+    Task(
+        "Cook food", "make rice", "14/01/2025", "20mins", "in progress"
+    ),
+]
+
 def home(request):
     return HttpResponse('<h1> Task Flow Home working </h1>')
 
 def about(request):
     return render(request, 'about.html')
 
+def task_index(request):
+    return render(request, 'tasks/index.html', {'tasks': tasks})
 # Create your views here.
