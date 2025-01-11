4ff93c42ae5630960b5901489a776646f8de4f74
Eddie <edwardchen_97@hotmail.com>
2025-01-11 13:19:52 +1100

    index.html css working

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,5 @@ from .import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('tasks/', views.cat_index, name='task-index')
+    path('tasks/', views.task_index, name='task-index')
 ]
