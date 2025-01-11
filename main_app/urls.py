cd60b507b180be0517dc1e28457efc3a42840be9
Eddie <edwardchen_97@hotmail.com>
2025-01-11 15:47:31 +1100

    detail.html working

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from .import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('tasks/', views.task_index, name='task-index')
+    path('tasks/', views.task_index, name='task-index'),
+    path('tasks/<int:task_id>/', views.task_detail, name='task-detail' )
 ]
