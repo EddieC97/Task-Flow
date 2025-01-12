f749e197f01c73711682c0a8f27f25910bec7f18
Eddie <edwardchen_97@hotmail.com>
2025-01-12 15:34:41 +1100

    update models.py to have MTM relationship

--- main_app/models.py
+++ main_app/models.py
@@ -1,30 +1,6 @@
 from django.db import models
 from django.urls import reverse
 
-STATUS = (
-    ("U", "Unassigned"), 
-    ("IP", "In Progress"), 
-    ("S", "Stuck"),
-    ("C", "Completed")
-    )
-
-class Task(models.Model):
-    name = models.CharField(max_length=250)
-    description = models.TextField(max_length=250)
-    date= models.DateField()
-    duration= models.IntegerField()
-    progress=  models.CharField(max_length=2, choices=STATUS, default=STATUS[0][0])
-
-    def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return reverse('task-detail', kwargs={'pk': self.id})
-
-    # * we are changing task_id to pk in the get_absolute_url because pk is Django's default parameter name for the primary key in URL patterns
-    # ~ Django's class-based views and URL routing expect 'pk' by default
-    # ~ using 'pk' ensures consistency with Django's conventions and avoids unnecessary complexity
-
 COLOR_CHOICES = (
     ("#FFFFFF", "White"),
     ("#FF5733", "Red"),
@@ -39,12 +15,41 @@ COLOR_CHOICES = (
     ("#FFEB3B", "Lime"),
 )
 
+
 class Tag(models.Model):
-    name=models.CharField(max_length=50)
-    color= models.CharField(max_length=8, choices=COLOR_CHOICES, default=COLOR_CHOICES[0][0])
-    
+    name = models.CharField(max_length=50)
+    color = models.CharField(
+        max_length=8, choices=COLOR_CHOICES, default=COLOR_CHOICES[0][0]
+    )
+
     def __str__(self):
         return self.name
-    
+
     def get_absolute_url(self):
-        return reverse('tag-detail', kwargs={"pk": self.id})
+        return reverse("tag-detail", kwargs={"pk": self.id})
+
+
+STATUS = (
+    ("U", "Unassigned"), 
+    ("IP", "In Progress"), 
+    ("S", "Stuck"),
+    ("C", "Completed")
+    )
+
+class Task(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.TextField(max_length=250)
+    date= models.DateField()
+    duration= models.IntegerField()
+    progress=  models.CharField(max_length=2, choices=STATUS, default=STATUS[0][0])
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('task-detail', kwargs={'pk': self.id})
+
+    # * we are changing task_id to pk in the get_absolute_url because pk is Django's default parameter name for the primary key in URL patterns
+    # ~ Django's class-based views and URL routing expect 'pk' by default
+    # ~ using 'pk' ensures consistency with Django's conventions and avoids unnecessary complexity
