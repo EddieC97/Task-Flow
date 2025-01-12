d8eff30b34a889e6a0a98a42d76a7dce358eaea3
Eddie <edwardchen_97@hotmail.com>
2025-01-12 19:13:19 +1100

    Logout button works + some CSS added

--- taskflow/settings.py
+++ taskflow/settings.py
@@ -120,6 +120,8 @@ STATIC_URL = 'static/'
 
 LOGIN_REDIRECT_URL = 'task-index'
 
+LOGOUT_REDIRECT_URL = 'home'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
