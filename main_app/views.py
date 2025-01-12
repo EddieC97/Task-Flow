eafc004827c18573a14e14c3d02b841755b5c8c6
Eddie <edwardchen_97@hotmail.com>
2025-01-12 16:21:03 +1100

    give tags working

--- main_app/views.py
+++ main_app/views.py
@@ -79,8 +79,10 @@ class TagDelete(DeleteView):
     success_url ='/tags/'
     
 def associate_tag(request, task_id, tag_id):
-    Task.objects.get(id=task_id).tags.add(tag_id)
-    return redirect('task-detail', task_id=task_id)
+    task = Task.objects.get(id=task_id)
+    tag = Tag.objects.get(id=tag_id)
+    task.tags.add(tag)
+    return redirect('task-detail', pk=task.id)
 
     # TODO
     # TODO 2. try-catch block for better error handling
