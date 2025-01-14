3642ce8806f693dea69c4e8617c29552a60a58d0
Eddie <edwardchen_97@hotmail.com>
2025-01-14 22:55:40 +1100

    trying render part 4

--- build.sh
+++ build.sh
@@ -3,11 +3,10 @@
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install pipenv
-pipenv install
+pip install -r requirements.txt
 
 # Convert static asset files
-pipenv run python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-pipenv run python manage.py migrate
+python manage.py migrate
