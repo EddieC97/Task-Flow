c3b352d5c71507fa7c63337f987b1eb19ccafa38
Eddie <edwardchen_97@hotmail.com>
2025-01-14 16:32:33 +1100

    reformat weeklyview.js + add comments for better understanding

--- main_app/static/js/weeklyview.js
+++ main_app/static/js/weeklyview.js
@@ -1,20 +1,26 @@
 function toggleForm(date) {
 	const formContainer = document.getElementById('task-form');
 	
-	const dateInput = document.querySelector("input[name='date']");
+    const dateInput = document.querySelector("input[name='date']");
+    
 
-	if (
-		formContainer.style.display === 'none' ||
-		formContainer.style.display === ''
-	) {
+    // * form initially will be display === none so when we click the + button: 
+    //*  1. we change the display to block to show it 
+    //*  2. we assign the date value to the date that is passed into the function 
+	if (formContainer.style.display === 'none') {
 		formContainer.style.display = 'block';
-		
-		dateInput.value = date;
-	} else {
+        dateInput.value = date;
+	}
+    //* form initially will be display === block so when we click the + button: 
+    // * 1. we change the display back to 'none' to hide it 
+    // * 2. we then clear the dateInput value to return to default  
+	if (formContainer.style.display === 'block') {
 		formContainer.style.display = 'none';
-		
 		dateInput.value = '';
 	}
 }
 
-// * 
+// * the const formContainer = document.getElementById('task-form'): 
+//* is used to find the <div> which contains the form so we can change the style.display 
+
+// * the dateInput is used to find the <input> field in the form that is called name='date' so we can reassign the value later with date 
