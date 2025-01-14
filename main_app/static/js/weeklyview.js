c5d191ed8ad13b89b543036da94a117031ceb378
Eddie <edwardchen_97@hotmail.com>
2025-01-14 16:48:08 +1100

    refactored back 2 if statement to if else + add more comments

--- main_app/static/js/weeklyview.js
+++ main_app/static/js/weeklyview.js
@@ -3,24 +3,21 @@ function toggleForm(date) {
 	
     const dateInput = document.querySelector("input[name='date']");
     
-
-    // * form initially will be display === none so when we click the + button: 
-    //*  1. we change the display to block to show it 
-    //*  2. we assign the date value to the date that is passed into the function 
 	if (formContainer.style.display === 'none') {
 		formContainer.style.display = 'block';
         dateInput.value = date;
-	}
-    //* form initially will be display === block so when we click the + button: 
-    // * 1. we change the display back to 'none' to hide it 
-    // * 2. we then clear the dateInput value to return to default  
-	if (formContainer.style.display === 'block') {
+        
+	} else {
 		formContainer.style.display = 'none';
 		dateInput.value = '';
 	}
+    
 }
 
 // * the const formContainer = document.getElementById('task-form'): 
 //* is used to find the <div> which contains the form so we can change the style.display 
 
 // * the dateInput is used to find the <input> field in the form that is called name='date' so we can reassign the value later with date 
+
+
+//! tried to do 2 if statement but it won't work because it gets checked twice
