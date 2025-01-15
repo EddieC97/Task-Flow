3a61f5a647611010890daea2a1e763462876348a
Eddie <edwardchen_97@hotmail.com>
2025-01-15 19:35:21 +1100

    add .cloneNode to copy DOM element

--- main_app/static/js/index.js
+++ main_app/static/js/index.js
@@ -1,34 +1,44 @@
 function filterTasks() {
 	const searchInput = document.getElementById('searchbar');
-	const query = searchInput.value.toLowerCase().replace(' ', '')
+	const query = searchInput.value.toLowerCase().replace(' ', '');
 
-	const taskList = document.querySelectorAll('.task-item');
+    const taskList = document.querySelectorAll('.task-item'); 
 	const searchResults = document.getElementById('search-results');
 
-	const filteredTasks = [];
+    const filteredTasks = [];
+    
+    
+
+	searchResults.innerHTML = '';
+	//* this line clears the whole div content in the beginning
 
 	taskList.forEach((task) => {
-        const taskName = task.querySelector('h2').textContent.toLowerCase().replace(' ','')
-        //* I am looking for the text content inside the <h2> which is usually the task.name
-        //* textContent then extracts the text inside the <h2> : get a string 
+		const taskName = task
+			.querySelector('h2')
+			.textContent.toLowerCase()
+			.replace(' ', '');
+		//* I am looking for the text content inside the <h2> which is usually the task.name
+		//* textContent then extracts the text inside the <h2> : get a string
 
 		if (taskName.includes(query)) {
 			filteredTasks.push(task);
 		}
 	});
 
-    searchResults.innerHTML = '';
-    //* this line clears the whole div content in the beginning 
-
 	if (filteredTasks.length > 0) {
-		filteredTasks.forEach((task) => {
-			searchResults.appendChild(task);
-        });
+        filteredTasks.forEach((task) => {
+            const cloneTask = task.cloneNode(true)
+			searchResults.appendChild(cloneTask);
+		});
         // * the task elements in the filteredTasks are not just plain string but are actually DOM elements: in our case <a> tag
+        // * in this case, we need to copy the task because I am directly manipulating the DOM
+        // ! without cloneNode, I will basically move the task from the all tasks list to the searchResult container like a true filter 
+        // ~ .cloneNode(true) basically makes a deep copy of the task 
 	} else {
-        const noTasksFoundMessage = document.createElement('p');
-        //* this creates a <p> element 
-		noTasksFoundMessage.textContent = 'No tasks found matching your query.';
+		const noTasksFoundMessage = document.createElement('p');
+		//* this creates a <p> element
+		noTasksFoundMessage.textContent =
+			'No tasks found matching your query.';
 		searchResults.appendChild(noTasksFoundMessage);
 	}
 }
