0aebcebaf28f259926e9012420f37827c32d885e
Eddie <edwardchen_97@hotmail.com>
2025-01-15 20:27:29 +1100

    refactor code with .trim()

--- main_app/static/js/index.js
+++ main_app/static/js/index.js
@@ -11,7 +11,7 @@ function clearSearchResults() {
 
 function filterTasks() {
 	const searchInput = document.getElementById('searchbar');
-	const query = searchInput.value.toLowerCase().replace(' ', '');
+	const query = searchInput.value.toLowerCase().trim();
 
     const taskList = document.querySelectorAll('.task-item'); 
     const searchResults = document.getElementById('search-results');
@@ -23,7 +23,7 @@ function filterTasks() {
 		const taskName = task
 			.querySelector('h2')
 			.textContent.toLowerCase()
-			.replace(' ', '');
+			.trim();
 		//* I am looking for the text content inside the <h2> which is usually the task.name
 		//* textContent then extracts the text inside the <h2> : get a string
 
@@ -53,3 +53,4 @@ function filterTasks() {
 
 // * .include(): I used this instead of === because I am looking for a partial match 
 // * the .replace(' ', ''): basically taking out spaces 
+// * initially I had .replace() but I changed to .trim() for removing whitespaces 
